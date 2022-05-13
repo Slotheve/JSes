abfe20839a4583424801c36e820cb479ef75e820
Slotheve <85428225+Slotheve@users.noreply.github.com>
2022-05-13 20:52:46 +0800

    upload

--- Module/dns.js
+++ Module/dns.js
@@ -1,5 +1,26 @@
-if ($network.wifi.ssid === 'Slotheve-5G' || $network.wifi.ssid === 'Slotheve-2.4G') {
-$done({servers:$network.dns})
-} else {
-$done({})
+const ssid1 = "Slotheve-5G";
+const ssid2 = "Slotheve-2.4G";
+const name = "WiFi-DNS";
+let home = ($network.wifi.ssid === ssid1) || ($network.wifi.ssid === ssid2);
+
+const getModuleStatus = new Promise((resolve) => {
+  $httpAPI("GET", "v1/modules", null, (data) =>
+	  resolve(data.enabled.includes(name))
+  );
+});
+
+getModuleStatus.then((enabled) => {
+  if (home && enabled) {
+	$notification.post(`关 ${name} 模块`, "" ,"");
+	enableModule(false);
+  } else if (!home && !enabled) {
+	$notification.post(`开 ${name} 模块`, "" ,"");
+	enableModule(true);
+  } else {
+	$done();
+  }
+});
+
+function enableModule(status) {
+  $httpAPI("POST", "v1/modules", { [name]: status }, () => $done());
 }
