d8c0d02522e53119fd12820873e45233657b5f90
Slotheve <85428225+Slotheve@users.noreply.github.com>
2022-05-13 19:34:04 +0800

    upload

--- Module/dns.js
+++ Module/dns.js
@@ -1,31 +1,5 @@
-const dns_module_home = 'WiFi DNS';
-const dns_module_out = 'Cellular DNS';
-const home = $network.wifi.ssid === 'Slotheve-5G' || $network.wifi.ssid === 'Slotheve-2.4G';
- 
-function getModuleStatus() {
-  return new Promise((resolve) => {
-    $httpAPI('GET', 'v1/modules', null, (data) => {
-      let enabled = data.enabled;
-      resolve([enabled.includes(dns_module_home), enabled.includes(dns_module_out)]);
-    });
-  });
+if ($network.wifi.ssid === 'Slotheve-5G' || $network.wifi.ssid === 'Slotheve-2.4G') {
+$done({servers:$network.dns})
+} else {
+$done({})
 }
- 
-const switchModule = (enable_module_name, disable_module_name) => {
-  $httpAPI('POST', 'v1/modules', {
-    [enable_module_name]: true,
-    [disable_module_name]: false,
-  }, () => $done());
-}
- 
-getModuleStatus().then((module_status) => {
-  if (home && (!module_status[0] || module_status[1])) {
-    $notification.post('WiFi DNS', '', '')
-    switchModule(dns_module_home, dns_module_out);
-  } else if (!home && (module_status[0] || !module_status[1])) {
-    $notification.post('数据 DNS', '', '')
-    switchModule(dns_module_out, dns_module_home);
-  } else {
-    $done();
-  }
-})
