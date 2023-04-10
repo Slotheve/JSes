2fc5bbf90a1933ffd3d64b04b594586d813bfeec
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-04-10 22:50:50 +0800

    Create Github.js

---
+++ Github.js
@@ -0,0 +1,19 @@
+let config = {
+  username: "Slotheve",
+  token: "custom",
+};
+
+// load user prefs from box
+const boxConfig = $persistentStore.read("github_private_repo");
+if (boxConfig) {
+  config = JSON.parse(boxConfig);
+}
+
+const username = $request.url.match(
+  /https:\/\/(?:raw|gist)\.githubusercontent\.com\/([^\/]+)\//
+)[1];
+// rewrite headers for specific user
+if (username == config.username) {
+  console.log(`ACCESSING PRIVATE REPO: ${$request.url}`);
+  $done({ headers: {...$request.headers, Authorization: `token ${config.token}`} });
+} else $done({});
