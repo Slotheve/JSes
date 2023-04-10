b8d5deb386a0a3ffd636b934ee551eab57227ec8
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-04-10 12:30:20 +0800

    Update WiFi.js

--- WiFi.js
+++ WiFi.js
@@ -1,3 +1,24 @@
+const WIFI_DONT_NEED_PROXYS = ['Slotheve'];
+const CURRENT_WIFI_SSID_KEY = 'current_wifi_ssid';
+
+if (wifiChanged()) {
+  const mode = WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)
+    ? 'direct'
+    : 'rule';
+  const 模式 = WIFI_DONT_NEED_PROXYS.includes($network.wifi.ssid)
+    ? '直连'
+    : '规则';
+  $surge.setOutboundMode(mode);
+  $notification.post(`出站 → ${模式}`, ``, ``);
+}
+
+function wifiChanged() {
+  const currentWifiSSid = $persistentStore.read(CURRENT_WIFI_SSID_KEY);
+  const changed = currentWifiSSid !== $network.wifi.ssid;
+  changed && $persistentStore.write($network.wifi.ssid, CURRENT_WIFI_SSID_KEY);
+  return changed;
+}
+
 const ssid = "Slotheve";
 const name = "DNS";
 let home = ($network.wifi.ssid === ssid);
