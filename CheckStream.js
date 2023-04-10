dde24ae0d42236842da773ed3bb930f36ea09713
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-04-10 13:19:27 +0800

    Update and rename check.js to CheckStream.js

--- CheckStream.js
+++ CheckStream.js
@@ -1,10 +1,4 @@
-/*
- * 由@LucaLin233编写
- * 原脚本地址：https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Surge/JS/stream-all.js
- * 由@Rabbit-Spec修改
- * 更新日期：2022.06.26
- * 版本：2.2
- */
+// 原脚本地址：https://raw.githubusercontent.com/LucaLin233/Luca_Conf/main/Surge/JS/stream-all.js
 
 const REQUEST_HEADERS = {
     'User-Agent':
