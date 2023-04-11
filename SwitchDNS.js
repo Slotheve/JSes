f36bced17a63eb0c000ec9ec8abc9eba6c48338c
Slotheve <85428225+Slotheve@users.noreply.github.com>
2023-04-11 08:03:37 +0800

    Update SwitchDNS.js

--- SwitchDNS.js
+++ SwitchDNS.js
@@ -1,7 +1,15 @@
-if ($network.wifi.ssid === 'Slotheve' ) {
-$notification.post(`dns → system`, "" ,"");
-$done({servers:$network.dns})
-} else {
-$notification.post(`dns → cunstom`, "" ,"");
-$done({})
+const wifi = 'Slotheve';
+
+function wifiChanged() {
+  const changed = $current_wifi_ssid !== $network.wifi.ssid;
+  changed && $persistentStore.write($network.wifi.ssid, $current_wifi_ssid);
+  return changed;
+}
+
+if (wifiChanged()) {
+  const 方式 = wifi.includes($network.wifi.ssid)
+    ? 'System'
+    : 'Cunstom';
+  $notification.post(`DNS → ${方式}`, ``, ``);
+  $done({servers:$network.dns})
 }
